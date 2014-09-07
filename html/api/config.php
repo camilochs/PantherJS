e07fe98b66d3e96a394530b3e0f4002a4ffcbf0e
Camilo Chacón Sartori <camilochs@gmail.com>
2014-09-06 22:53:06 -0400

    Revert b9a6623..c6a2385
    This rolls back to commit b9a6623648f2eac2a6a495823e2bc467cfc0ee18.

---
+++ html/api/config.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Your Twitter App Info
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'YOUR-CONSUMER_KEY_HERE');
+    define('CONSUMER_SECRET', 'YOUR-CONSUMER_SECRET_HERE');
+
+    // User Access Token
+    define('ACCESS_TOKEN', 'YOUR-ACCESS_TOKEN_HERE');
+    define('ACCESS_SECRET', 'YOUR-ACCESS_SECRET_HERE');
